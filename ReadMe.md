edce251f5690944b1949dba2aa5d187d3bc047eb
A192747 <petr.khuraskin@yandex.com>
2024-12-09 19:57:32 +0300

    Dev and prod + Dockerfile added

--- ReadMe.md
+++ ReadMe.md
@@ -2,8 +2,19 @@
 ```bash
 ./start.sh
 ```
-### Документация к api находится по адресу (**будущем адрес изменится**): </br>
-http://localhost:8081/api/v1/swagger-ui/index.html#/
+
+## Для разработки (postgres, kafka, zoo и  keycloak)
+```bash
+./start.sh -dev
+```
+
+## Для остановки всех докер контейнеров
+```bash
+./start.sh -down-all
+```
+
+### Документация к api находится по адресу: </br>
+http://localhost/api/v1/swagger-ui/index.html
 
 ### Архитектура проекта
 ![Screenshot](https://github.com/A192747/VKR-Career-Development-Platform/blob/develop/Info/images/architecture.jpg)
--- start.sh
+++ start.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
+
 CURRENT_DIR=${PWD}
 
+# Останавливаем все docker-compose
+echo "Stopping docker-compose from all dirs..."
 cd Docker || exit
 docker-compose down
-docker-compose up -d
+cd dev || exit
+docker-compose down
+
+cd "$CURRENT_DIR" || exit
+
+# Если передан ключ -dev, выполняем только docker-compose для development
+if [[ "$1" == "-dev" ]]; then
+    echo "Starting docker-compose for development..."
+    cd Docker || exit
+    cd dev || exit
+    docker-compose up -d
+else
+    if [[ "$1" == "-down-all" ]]; then
+        echo "Done"
+    else
+        echo "Starting docker-compose for full project..."
+        cd Docker || exit
+        docker-compose up -d
+    fi
+fi
+
 cd "$CURRENT_DIR" || exit
